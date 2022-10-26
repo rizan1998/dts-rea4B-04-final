04722a6f6a9c7e8370cac5b0e446ade3d280fab4
rizan1998 <rizanalfalah@gmail.com>
2022-10-26 20:45:54 +0700

    optimize card anime title

--- src/Components/Home/CardAnime.js
+++ src/Components/Home/CardAnime.js
@@ -22,13 +22,11 @@ function CardAnime(item) {
         <Card className="cardParent" sx={{ maxWidth: 345 }}>
           <CardMedia component="img" height="190" image={item.item.images.jpg.image_url} alt="green iguana" />
           <CardContent style={{ height: "70px" }}>
-            <Typography gutterBottom sx={{ fontSize: "12px" }} variant="p" component="div">
+            <Typography gutterBottom sx={{ fontSize: "15px" }} variant="p" component="div">
               {item.item.title}
             </Typography>
-            <Typography variant="body2" color="text.secondary">
-              {/* Rating : {item.item.rating} */}
-            </Typography>
-            <Typography sx={{ marginTop: "10px" }} variant="body2" color="text.secondary">
+            <Typography variant="body2" color="text.secondary"></Typography>
+            <Typography sx={{ marginTop: "10px", fontSize: "12px" }} variant="p" color="text.secondary">
               Genre: {genres}
             </Typography>
           </CardContent>
